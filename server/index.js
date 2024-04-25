e7df2164dc362e322efac5579de64b1bd0df551e
starkz24 <aman.kumarz2404@gmail.com>
2024-04-25 18:54:14 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+const routes = require('./router/routes');  
+
+const app = express();
+const PORT = process.env.PORT || 1337;
+const MONGO_URI = process.env.MONGO_URI ;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb+srv://amankumarz2404:callofduty@cluster0.lf2rgu2.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
