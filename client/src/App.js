7b50c79aa0a61b86d5ab42f7d5c86da3543c0853
aman <amankumar@Amans-MacBook-Air.local>
2025-12-03 19:04:48 +0530

    register updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,39 @@
+// import React,{ useState } from "react";
+// import './index.css';
+// import Display from './Components/Dashboard/Display';
+// import Mainpage from './Components/Mainlanding/Mainpage';
+// import { Route,Routes } from "react-router-dom";
+// import Events from './Components/events/Events'
+// import Leaderboard from "./Components/LeaderBoard/Leaderboard";
+// import Donation from "./Components/Dono Page/Donation";
+
+
+// const App = () => {
+  
+//   const [token, setToken] = useState(false);
+
+//   if(!token){
+//     return <Mainpage setToken={setToken}/>
+//   }
+
+
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path="/" element={<Display/>}/>
+//         {/* <Route path="/logout" element={<Logout/>}/> */}
+//         <Route path="/event" element={<Events/>}/>
+//         <Route path="/leaderboard" element={<Leaderboard/>}/>
+//         <Route path="/donate" element={<Donation/>}/>
+//       </Routes>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
 import React, { useState } from "react";
 import './index.css';
 import Display from './Components/Dashboard/Display';
@@ -6,6 +42,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Events from './Components/events/Events';
 import Leaderboard from "./Components/LeaderBoard/Leaderboard";
 import Donation from "./Components/Dono Page/Donation";
+import Signup from "./Components/Mainlanding/Register";
 
 const App = () => {
 
@@ -17,6 +54,7 @@ const App = () => {
     <Routes>
 
       <Route path="/" element={<Mainpage setToken={setToken} />} />
+      <Route path="/register" element={<Signup />}/>
 
       <Route path="/dashboard" element={token ? <Display /> : <Navigate to="/" />} />
 
--- client/src/Components/Mainlanding/Mainpage.jsx
+++ client/src/Components/Mainlanding/Mainpage.jsx
@@ -1,35 +1,85 @@
-import React from "react";
+// import React from "react";
+// import Title from "./Title";
+// import Register from "./Register";
+// import Login from "./Login";
+// import Landing from "./Landing";
+// import Pioneer from "./Pioneer";
+// import { Routes, Route} from 'react-router-dom';
+// import Footer from "../Dashboard/Footer";
+
+// const Mainpage = ({setToken}) => {
+
+//   return (
+//     <div className="flex flex-col justify-around">
+//       <div class="md:flex md:justify-between">
+//         <div class="hidden md:block">
+//           <Landing />
+//         </div>
+//         <div class="flex flex-col mx-auto w-11/12 md:w-80 md:mr-14">
+//           <div class="hidden md:block">
+//             <Title />
+//           </div>
+//           <div class="md:hidden">
+//             <Pioneer />
+//           </div>
+//           <div>
+//             <Routes>
+//               <Route path="/" element={<Login setToken={setToken} />} />
+//               <Route path="/register" element={<Register/>} />
+//             </Routes>
+//           </div>
+//         </div>
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default Mainpage;
+
+
+import React, { useState } from "react";
 import Title from "./Title";
 import Register from "./Register";
 import Login from "./Login";
 import Landing from "./Landing";
 import Pioneer from "./Pioneer";
-import { Routes, Route} from 'react-router-dom';
 import Footer from "../Dashboard/Footer";
 
-const Mainpage = ({setToken}) => {
+const Mainpage = ({ setToken }) => {
+
+  const [showRegister, setShowRegister] = useState(false);
 
   return (
     <div className="flex flex-col justify-around">
-      <div class="md:flex md:justify-between">
-        <div class="hidden md:block">
+      <div className="md:flex md:justify-between">
+
+        <div className="hidden md:block">
           <Landing />
         </div>
-        <div class="flex flex-col mx-auto w-11/12 md:w-80 md:mr-14">
-          <div class="hidden md:block">
+
+        <div className="flex flex-col mx-auto w-11/12 md:w-80 md:mr-14">
+
+          <div className="hidden md:block">
             <Title />
           </div>
-          <div class="md:hidden">
+
+          <div className="md:hidden">
             <Pioneer />
           </div>
+
+          {/* SWITCH BETWEEN LOGIN & REGISTER */}
           <div>
-            <Routes>
-              <Route path="/" element={<Login setToken={setToken} />} />
-              <Route path="/register" element={<Register/>} />
-            </Routes>
+            {!showRegister ? (
+              <Login setToken={setToken} setShowRegister={setShowRegister} />
+            ) : (
+              <Register setShowRegister={setShowRegister} />
+            )}
           </div>
+
         </div>
       </div>
+
       <Footer />
     </div>
   );
